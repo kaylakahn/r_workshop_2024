005bdeca26f38e38041797537abb705a6fd7621f
kkahn1 <54557269+kkahn1@users.noreply.github.com>
2024-08-22 15:43:55 -0400

    some changes

--- code/Day 5 tidyverse dplyr.R
+++ code/Day 5 tidyverse dplyr.R
@@ -15,20 +15,21 @@
 # use "" instead of '' unless you need to quote inside a quote
 # You will pick this up as you read others' example code.
 # Most importantly, pick something and stick with it.
-# e.g. Within one script don't randomly switch naming conventions or the way you do spacing
+# i.e. Within one script don't randomly switch naming conventions or the way you do spacing
 
 
 # Today we will learn about data wrangling with the tidyverse, with attention paid to dplyr
-# Extremely good tidyverse guide https://modern-rstats.eu/descriptive-statistics-and-data-manipulation.html - chapter 4
+# Extremely good tidyverse guide/tutorial https://modern-rstats.eu/descriptive-statistics-and-data-manipulation.html - chapter 4
 library(dplyr)
 library(lubridate)
 
 # We will return to the dataset we worked with on Tuesday
 office_df <- read.csv("the_office_series.csv", na.strings = "")
+# turning column names to lowercase (personal preference)
 names(office_df) <- tolower(names(office_df))
 
 # select() is for columns and filter() is for rows
-# We removed column X by using office_df$X <- NULL
+# Previously we removed column X by using office_df$X <- NULL
 # But we can use dplyr
 office2 <- select(office_df, !x)
 # But something amazing about dplyr is the pipe %>%
@@ -37,6 +38,9 @@ office2 <- office_df %>%
 # Or you could select all columns except the first
 office2 <- office_df %>%
   select(season:writers)
+# another way to select all columns except the first
+office2 <- office_df %>%
+  select(-1)
 # Maybe we only want numeric columns
 office2 <- office_df %>%
   select(where(is.numeric))
@@ -59,15 +63,21 @@ mindy_df <- office_df %>%
 
 # We can chain operations. This is when the pipe becomes useful
 # Let's say we want ratings above 8, and we only want columns episodetitle, about, and director
+# base R: df[df$column > 8, c("column_you_want_to_keep", "other_column_you_want_to_keep")]
+office_df[office_df$ratings > 8, c("episodetitle", "about", "director")]
+# dplyr
 office2 <- office_df %>%
   select(episodetitle, about, director) %>%
   filter(ratings > 8)
 # What does the error tell us? Why?
+# dplyr correct way
 office2 <- office_df %>%
   filter(ratings > 8) %>%
   select(episodetitle, about, director) 
 
 # We can rename too
+# base R: names(df)[names(df) == "current_var_name"] <- "new_var_name"
+# dplyr: rename(df, new = old) (or with the pipe: df %>% rename(new = old))
 office2 <- office_df %>%
   filter(ratings > 8) %>%
   select(episodetitle, about, director) %>%
@@ -81,25 +91,29 @@ office_df %>%
 # That isn't too informative. It might be better to get the mean and sd of ratings by year
 # First, we need a year column
 office_df$year <- year(dmy(office_df$date))
-# Now we can use dplyr to group by the year
+# Now we can use dplyr to group by the year 
 office_year_ratings <- office_df %>%
   group_by(year) %>%
   summarize(mean = mean(ratings), max = max(ratings))
 
 office_year_ratings
-# This output is a "tibble" and not a dataframe
+# Note that this output is a "tibble" and not a dataframe
 # In many circumstances, functions treat the two the same. In other cases...
+# So you just use as.data.frame() to change it
 as.data.frame(office_year_ratings)
 
-# Return to summarize()
-# We want to summarise a lot of columns! Let's try it with just one function first
+# Returning to summarize()
+# We want to summarize a lot of columns! Let's try it with just one function first
 # We will use the function across() inside summarize()
-# And earlier, we actually named something mean, so we need to remove it so R recognizes the function
 rm(mean)
 office_df %>%
   group_by(year) %>%
   summarize(across(ratings:duration, .fns = mean, .names = "mean_{col}"))
 # Use {} to dynamically refer to columns instead of typing them out
+# If it doesn't run, you may have called something "mean" in your environment
+rm(mean) # to get rid of it/restore mean() as a function, then try running again
+
+
 
 # Here are some other very similar things we can do
 # Summarize across all numeric columns
@@ -110,9 +124,17 @@ office_df %>%
 # Summarize across all columns starting with a certain letter
 # (first we will need to rename some columns to illustrate what happens)
 office_df %>%
+  # rename cols from "rating" to "duration" so that they start with "k_"
+  # ~ here is for an anonymous function. Telling R that we're using a formula applied across some cols
   rename_with(.fn = ~ paste0("k_", .), .cols = c(ratings:duration)) %>%
   group_by(year) %>%
   summarize(across(starts_with("k_"), .fns = mean, .names = "mean_{col}"))
+# If the paste thing doesn't make much sense, that's OK!!! 
+# The illustration here is more so that you know rename_with exists
+# there are other ways 
+# names(office_df)[which(names(office_df) %in% c("ratings", "votes", "viewership", "duration"))] <- paste0("k_", names(office_df)[which(names(office_df) %in% c("ratings", "votes", "viewership", "duration"))])
+# or typing each out in rename
+# but these can become tedious, which is why I want you to know the rename_with() option exists! 
 
 # What if we want to apply more than one function?
 office_df %>%
@@ -121,12 +143,14 @@ office_df %>%
 
 # Grouping by more than one column
 office_df %>%
+  # group by year and month. month(dmy(date)) is there instead of creating a $month col. 
   group_by(year, month(dmy(date))) %>%
-  arrange(year, month(dmy(date))) %>% # sort by years then months
+  # sort by years then months
+  arrange(year, month(dmy(date))) %>% 
   summarize(across(ratings:duration, list(mean = mean, max = max, min = min), .names = "{.fn}_{.col}"))
 
-# Applying different functions to different columns
-# We want all episode titles and all writers and we just want to separate them by ;
+# Applying different functions to different columns when summarizing
+# We want to maintain all episode titles and all writers and we just want to separate them by ;
 # We want the highest rating and viewership for that year-month
 # And we want the mean votes
 # And the first about and first director
@@ -136,14 +160,15 @@ office_df_yrmonth <- office_df %>%
   group_by(year, month(dmy(date))) %>%
   summarise(across(.cols = c(episodetitle, writers), .fns = ~ paste(., collapse = "; ")),
             across(.cols = c(ratings:viewership), .fns = max),
-            across(.cols = votes, .fns = mean), # could also use mean(votes)
+            across(.cols = votes, .fns = mean), # could also use mean(votes) since there's only one
             across(.cols = c(about, director), .fns = first),
-            num_eps = n()
+            num_eps = n() # total number of episodes in the month-year
             )
-# Side notes, not super important to know right now:
+
 # ~ in this sense is different than ~ in regression models
 # ~ here is for an anonymous function. Telling R that we're using a formula
-# . refers to the current value of the column. or you can also think of it as referring to itself
+# . refers to the current value of the column. or you can also think of it as referring to itself 
+# (paste itself and separate each by a ;)
 
 
 # mutate() is used to create new columns
